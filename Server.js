59b371785a6eada58858712878851b3dd348f1e2
Nikhil Singh <30321610+ImSnik@users.noreply.github.com>
2018-10-21 14:13:11 +0530

    Update for advanced templating
    Repetitive set of code such as getting current year in the previous file can be replaced with Helpers - a kind of function which reduces the task of repeating the code again and again
    -- partials are also required to be registered in order to be used inside several views

--- Server.js
+++ Server.js
@@ -3,21 +3,23 @@ const hbs = require('hbs');   // handle bars
 
 var app = express();
 app.set('view engine', 'hbs');   //setting view engine to handle bars
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
 
 app.get('/', (req, res) => {
-    res.send({
-        name: 'Snapnab',
-        age: 4
+    res.render('home.hbs', {
+        titlePage : 'Home Page'
     });
 });
 
 app.get('/about', (req,res) => {
     res.render('about.hbs', {         //rendering the about.hbs file in the views folder
-        titlePage: 'Title Page',
-        currentYear: new Date().getFullYear()     //getting the current date and using it in the template string placed in the about.hbs
+        titlePage: 'Title Page'
     });
 });
 
